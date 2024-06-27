50985d23f4530baba29855ab75d9cfd30e163800
Maël KEMPF-LE PAPE <mael.kempf-le-pape@epitech.eu>
2024-06-27 15:20:41 +0200

    [ADD] resize window : OK

--- js/main.js
+++ js/main.js
@@ -1,19 +1,24 @@
-const scene = new THREE.Scene();
+let scene, camera, cube, renderer;
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+function init()
+{
+  scene = new THREE.Scene();
 
-const renderer = new THREE.WebGLRenderer();
-
-renderer.setSize(window.innerWidth, window.innerHeight);
-
-document.body.appendChild(renderer.domElement);
-
-const geometry = new THREE.BoxGeometry(2, 2, 2); 
-const material = new THREE.MeshBasicMaterial({ color: 0x0000ff }); 
-const cube = new THREE.Mesh(geometry, material); 
-scene.add(cube);
-
-camera.position.z = 5;
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  
+  renderer = new THREE.WebGLRenderer({ antialias : true});
+  
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  
+  document.body.appendChild(renderer.domElement);
+  
+  const geometry = new THREE.BoxGeometry(2, 2, 2); 
+  const material = new THREE.MeshBasicMaterial({ color: 0x0000ff }); 
+  cube = new THREE.Mesh(geometry, material); 
+  scene.add(cube);
+  
+  camera.position.z = 5;
+}
 
 function animate() {
   requestAnimationFrame(animate);
@@ -24,4 +29,14 @@ function animate() {
   renderer.render(scene, camera);
 }
 
+function resize()
+{
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+window.addEventListener("resize", resize, false);
+
+init();
 animate();
